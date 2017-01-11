2e1b2266e6734e565490b83ef3cb34381112bdf3
Ian Kent <iank1226@yahoo.com>
2017-01-11 14:48:51 -0500

    added new files

--- compile.bat
+++ compile.bat
@@ -2,4 +2,6 @@ javac unionfind\QuickFindUF.java
 javac unionfind\QuickUnion.java
 javac unionfind\WeightedQuickUnion.java
 javac unionfind\Init.java
-javac TestUF.java
+javac TestQF.java
+javac TestQU.java
+javac TestWQU.java
