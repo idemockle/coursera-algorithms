730adaa5b792eef7452d197cf578de99a7299939
Ian Kent <iank1226@yahoo.com>
2017-01-11 16:48:47 -0500

    Percolation class specification and constructor

---
+++ Assignment 1/Percolation.java
@@ -0,0 +1,17 @@
+public class Percolation {
+   int[][] parents;
+   
+   public Percolation(int n) {               // create n-by-n grid, with all sites blocked
+      parents = int[n][n];
+      for (int i = 0, j = 0; i < n; i++, j++) {
+         parents[i][j] = null;
+      }
+   }
+   // public    void open(int row, int col)    // open site (row, col) if it is not open already
+   // public boolean isOpen(int row, int col)  // is site (row, col) open?
+   // public boolean isFull(int row, int col)  // is site (row, col) full?
+   // public     int numberOfOpenSites()       // number of open sites
+   // public boolean percolates()              // does the system percolate?
+
+   // public static void main(String[] args)   // test client (optional)
+}
