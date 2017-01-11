086d48fa1d3255b782d31b8eb5592f0c6628c30a
Ian Kent <iank1226@yahoo.com>
2017-01-11 14:14:53 -0500

    added WeightedQuickUnion

---
+++ unionfind/WeightedQuickUnion.java
@@ -0,0 +1,33 @@
+package unionfind;
+
+public class WeightedQuickUnion extends QuickUnion {
+    int[] treeSize;
+    int[] id;
+    
+    public WeightedQuickUnion (int N) {
+        int idx;
+        
+        id       = new int[N];
+        treeSize = new int[N];
+        for (idx = 0; idx < N; idx++) {
+            id[idx] = idx;
+            treeSize[idx] = 1;
+        }
+    }
+    
+    public void union(int p, int q) {
+        int child;
+        int parent;
+        
+        if (treeSize[root(p)] <= treeSize[root(q)]) {
+            child = root(p);
+            parent = root(q);
+        } else {
+            child = root(q);
+            parent = root(p);
+        }
+        
+        id[child] = parent;
+        treeSize[parent]++
+    }
+}
