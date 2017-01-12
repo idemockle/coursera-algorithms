2cdc1ddff79999920c9f8ff04a647d20be806392
Ian Kent <iank1226@yahoo.com>
2017-01-12 09:38:00 -0500

    change array to 1d

--- Assignment 1/Percolation.java
+++ Assignment 1/Percolation.java
@@ -1,13 +1,19 @@
 public class Percolation {
-   int[][] parents;
+   int[] parents;
    
    public Percolation(int n) {               // create n-by-n grid, with all sites blocked
-      parents = new int[n][n];
+      parents = new int[n*n+2];
       for (int i = 0, j = 0; i < n; i++, j++) {
-         parents[i][j] = -1;
+         if (i <= n) parents[i] = 0;
+         else if (i >= n*(n-1)+1)
+            parents[i] = n**2+1;
+         else parents = -1;
       }
    }
-   // public    void open(int row, int col)    // open site (row, col) if it is not open already
+   public void open(int row, int col) { 
+      // open site (row, col) if it is not open already
+      
+   }   
    // public boolean isOpen(int row, int col)  // is site (row, col) open?
    // public boolean isFull(int row, int col)  // is site (row, col) full?
    // public     int numberOfOpenSites()       // number of open sites
