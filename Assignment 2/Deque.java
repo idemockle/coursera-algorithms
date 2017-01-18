5a2f28b672cd3f5182a4d6960dd35d5c201a7993
Ian Kent <iank1226@yahoo.com>
2017-01-18 17:33:45 -0500

    Deque file. Compiles; non-spec test code commented out.

--- Assignment 2/Deque.java
+++ Assignment 2/Deque.java
@@ -1,26 +1,169 @@
+// import java.io.*;
+// import java.nio.file.*;
+import java.util.Iterator;
+
 public class Deque<Item> implements Iterable<Item> {
+   private Node first;
+   private Node last;
+   private int N;
    
-   public Deque()                           // construct an empty deque
+   // public Deque() {
+      // construct an empty deque
+      // Do I even need this?
+   // }
    
-   public boolean isEmpty()                 // is the deque empty?
+   public boolean isEmpty() { return N == 0; }                // is the deque empty?
    
-   public int size()                        // return the number of items on the deque
+   public int size() { return N; } // return the number of items on the deque
    
-   public void addFirst(Item item)          // add the item to the front
+   public void addFirst(Item item) {
+      // add the item to the front
+      
+      if (item == null) {
+         throw new NullPointerException();
+      }
+      
+      Node oldfirst = first;
+      first = new Node();
+      first.next = oldfirst;
+      first.item = item;
+      if (N == 0) {
+         last = first;
+      } else if (N == 1) {
+         last.prev = first;
+      }
+      N++;
+   }
    
-   public void addLast(Item item)           // add the item to the end
+   public void addLast(Item item) {
+      // add the item to the end
+      
+      if (item == null) {
+         throw new NullPointerException();
+      }
+      
+      Node oldlast = last;
+      last = new Node();
+      last.prev = oldlast;
+      last.item = item;
+      if (N == 0) {
+         last = first;
+      } else if (N == 1) {
+         first.next = last;
+      }
+      N++;
+   }
    
-   public Item removeFirst()                // remove and return the item from the front
+   public Item removeFirst() {
+      // remove and return the item from the front
+      
+      if (N == 0) {
+         throw new java.util.NoSuchElementException();
+      }
+      
+      Node oldfirst = first;
+      first = first.next;
+      if (N == 2) {
+         last.prev = null;
+      } else if (N == 1) {
+         last = null;
+      }
+      N--;
+      return oldfirst.item;
+   }
    
-   public Item removeLast()                 // remove and return the item from the end
+   public Item removeLast() {
+      // remove and return the item from the end
+      
+      if (N == 0) {
+         throw new java.util.NoSuchElementException();
+      }
+      
+      Node oldlast = last;
+      last = last.prev;
+      if (N == 2) {
+         first.next = null;
+      } else if (N == 1) {
+         first = null;
+      }
+      N--;
+      return oldlast.item;
+   }
    
-   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+   public Iterator<Item> iterator() {
+      // return an iterator over items in order from front to end
+      return new ForwardIterator();
+   }
+   
+   private class ForwardIterator implements Iterator<Item> {
+      private Node current = first;
+      
+      public boolean hasNext() {
+         return current != null;
+      }
+      
+      public Item next() {
+         if (current == null) {
+            throw new java.util.NoSuchElementException();
+         }
+         Node oldcurrent = current;
+         current = current.next;
+         return oldcurrent.item;
+      }
+      
+      public void remove() {
+         /* not implemented */
+         throw new UnsupportedOperationException();
+      }
+      
+   }
    
    private class Node {
       Node next;
       Node prev;
       Item item;
+      
+      // public String toString() {
+         // String nextval;
+         // String prevval;
+         
+         // if (next == null) { nextval = "No node."; }
+         // else  { nextval = next.item.toString(); }
+         
+         // if (prev == null) { prevval = "No node."; }
+         // else { prevval = prev.item.toString(); }
+         
+         // return ("\n     Value: " + item + "\n" +
+                  // "Next Value: " + nextval + "\n" +
+                  // "Prev Value: " + prevval);
+      // }
    }
    
-   public static void main(String[] args)   // unit testing (optional)
+   // public static void main(String[] args) {
+      // Deque<Integer> deque = new Deque<Integer>();
+      
+      // /* Path file = Paths.get(args[0]);
+      // try (BufferedReader reader = Files.newBufferedReader(file)) {
+         // String line = null;
+         // while ((line = reader.readLine()) != null) {
+            // String[] strInts = line.split("\\s");
+            // for (String strNum : strInts) {
+               // deque.addFirst(Integer.parseInt(strNum));
+            // }
+         // }
+      // } catch (IOException x) {
+         // System.err.format("IOException: %s%n", x);
+      // } */
+      
+      // deque.addFirst(1);
+      // deque.addLast(2);
+      // deque.addFirst(3);
+      // System.out.println(deque.removeFirst());
+      // deque.removeFirst();
+      // deque.removeFirst();
+      
+      // System.out.println("---First Node---" + deque.first);
+      // System.out.println("---Last Node---" + deque.last);
+      
+   // }
 }
