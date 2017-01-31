dcb1ebe29c447247a276cba41c64e5123215b3a1
Ian Kent <iank1226@yahoo.com>
2017-01-31 12:15:43 -0500

    initial API

---
+++ Assignment 3/src/FastCollinearPoints.java
@@ -0,0 +1,11 @@
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 or more points
+    }
+    public int numberOfSegments(){
+        
+    }
+    public LineSegment[] segments() {
+        // the line segments
+    }
+}
